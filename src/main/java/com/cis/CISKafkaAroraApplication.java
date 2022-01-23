8daec7d2c5d97593b8ab8ce21c43a33812235ac7
DharmendraKachhawahaAtHarbinger <dharmendra.k@harbingergroup.com>
2022-01-23 18:15:28 +0530

    Naming convention changes

--- src/main/java/com/cis/CISKafkaAroraApplication.java
+++ src/main/java/com/cis/CISKafkaAroraApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class CISKafkaAroraApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+        SpringApplication.run(CISKafkaAroraApplication.class, args);
     }
 }
